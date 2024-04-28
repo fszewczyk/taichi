eaee968bb29cd5c88ee523aae5dbd8ee790dac16
Franciszek Szewczyk <szewczyk.franciszek02@gmail.com>
2024-04-28 13:07:25 +0200

    Update for newere macOS and Python in utils

--- python/taichi/misc/util.py
+++ python/taichi/misc/util.py
@@ -8,7 +8,7 @@ import time
 
 def get_os_name():
   name = platform.platform()
-  if name.lower().startswith('darwin'):
+  if name.lower().startswith('darwin') or name.lower().startswith('macos'):
     return 'osx'
   elif name.lower().startswith('windows'):
     return 'win'
